526fb74d6bbf389b628a9706269ebf337d386ab2
chen <zhjay999@163.com>
2020-04-16 08:14:11 +0800

    1

--- tasmota/xdrv_02_mqtt.ino
+++ tasmota/xdrv_02_mqtt.ino
@@ -181,14 +181,25 @@ void MqttInit(void)
    byte key[] = { 0x2B, 0x7E, 0x15, 0x16, 0x28, 0xAE, 0xD2, 0xA6, 0xAB, 0xF7, 0x15, 0x88, 0x09, 0xCF, 0x4F, 0x3C };
     
   // put your setup code here, to run once:
-  byte my_iv[16] = { 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
+  byte my_iv[N_BLOCK] = { 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
   aesLib.gen_iv(my_iv);
+
+  Serial.println("AES Key: ");
+  int i;
+  for (i = 0; i < sizeof(my_iv); i++)
+  {
+     Serial.print(my_iv[i], DEC); 
+     Serial.print(",");
+  }
+
+  Serial.println("");
 
   String msg = "i like dogs";
   
+  unsigned long ms = micros ();
   String encMsg = aesLib.encrypt(msg, key, my_iv);
 
-  AddLog_P2(LOG_LEVEL_INFO, PSTR(D_LOG_MQTT "TLS connection error: %d"), mqttport);
+  AddLog_P2(LOG_LEVEL_INFO, PSTR(D_LOG_MQTT "TLS connection error: %d, %s"), mqttport, encMsg.c_str());
 
   // AddLog_P2(LOG_LEVEL_INFO, S_LOG_MQTT, PSTR("init-start %d"), mqttport );
 #if defined(USE_MQTT_TLS)
